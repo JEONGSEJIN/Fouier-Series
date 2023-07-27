123970f5891799a13aaff9317c0c40aa01816c13
Sejin Jeong <luyttt4@gmail.com>
2023-07-28 06:17:15 +0900

    Add files via upload

---
+++ Fourier Series.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+	double n;
+	printf("nÀÇ °³¼ö:");
+	scanf("%f", &n);
+	double function=1.5;
+	double pi;
+	double x;
+    double sum = 1.0;
+
+	for (double i = 1; i < n; i++)
+	{
+		function = (1.5)*(2 - cos(i*pi) * 2)*sin(i*x);
+		sum += function;
+	}
+	printf("%f", sum);
+	return 0;
+}
